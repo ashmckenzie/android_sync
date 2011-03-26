b2196f308e81846a8ffc34ab6af3dc96d2ccefd9
Ashley McKenzie <ash@ashmckenzie.org>
2011-03-26 17:12:55 +1100

    rspec tests updated to achieve 100% rcov coverage

--- lib/android_sync.rb
+++ lib/android_sync.rb
@@ -12,6 +12,8 @@ end
 
 class AndroidSync
 
+  attr_accessor :debug, :verbose, :pretend
+
   # The default YAML configuration file
   #
   DEFAULT_CONFIG = File.dirname(__FILE__) + '/../android_sync.yml'
@@ -21,75 +23,102 @@ class AndroidSync
   DEFAULT_PRETEND = false
 
   DEFAULT_VERBOSE = true
-
   DEFAULT_DEBUG = false
 
-  def initialize(opts)  # :nodoc:
+  def initialize(opts = {})  # :nodoc:
 
-    @destination = opts[:destination]
-    @config_file = opts[:config] || DEFAULT_CONFIG
-    @pretend = opts[:pretend] || DEFAULT_PRETEND
-    @verbose = opts[:verbose] || DEFAULT_VERBOSE
-    @debug = opts[:debug] || DEFAULT_DEBUG
+    raise Exception, 'Destination must be defined' unless opts[:destination]
+
+    @destination_base = opts[:destination]
+
+    @config_file = opts[:config] ? opts[:config] : DEFAULT_CONFIG
+
+    raise Exception, 'Config file not found' unless File.exists?(@config_file)
+
+    @pretend = ! opts[:pretend].nil? ? opts[:pretend] : DEFAULT_PRETEND
+    @verbose = ! opts[:verbose].nil? ? opts[:verbose] : DEFAULT_VERBOSE
+    @debug = ! opts[:debug].nil? ? opts[:debug] : DEFAULT_DEBUG
 
     @config = YAML.load_file(@config_file)
+
+    @source = nil
+    @destination = nil
+    
+    @new_files = []
+    @new_files_destination = []
+
   end
 
   # Sync a bunch of stuff to an Android device (essentially a mounted volume)
   #
   def run
-
     puts
-
-    destination = @destination.trim('/')
-
-    @config['sources'].each do |source|
-
-      puts "- Looking at #{source['label']}"
-      
-      final_destination = eval('"' + source['destination'] + '"').trim('/') + '/'
-      FileUtils.mkdir_p(final_destination) unless File.directory?(final_destination) and @pretend
-
-      all_new_files = Dir.glob("#{source['source'].trim('/')}/*").sort { |x, y| File::stat(y).ctime <=> File::stat(x).ctime }
-
-      unless source['keep'].nil?
-        new_files = all_new_files[0...source['keep']]
-      else
-        new_files = all_new_files
-      end
-
-      sync_files(new_files, final_destination)
-
-      cleanup(final_destination, new_files) unless source['keep'].nil?
-
+    @config['sources'].each do |s|
+      puts "- Looking at #{s['label']}"
+      perform_sync(s['source'], s['destination'], s['keep'])
       puts
-
     end
+  end
+
+  def perform_sync(source, destination, keep=nil)
+    @new_files, @new_files_destination = get_new_files_to_sync(source, destination, keep)
+    sync_new_files
+    cleanup_files_we_dont_want_to_keep unless keep.nil?
+  end
+
+  def get_new_files_to_sync(source, destination, keep=nil)
+
+    @source = source
+    @destination = destination
+
+    # This variable is eval'd using entries setup in the YAML file
+    #
+    destination_base = @destination_base.trim('/')
+
+    new_files_destination = eval('"' + destination + '"').trim('/')
+
+    all_new_files = Dir.glob("#{source.trim('/')}/**/*").reject { |x| File.directory?(x) }.sort { |x, y| File::stat(y).ctime <=> File::stat(x).ctime }
+
+    unless keep.nil?
+      new_files = all_new_files[0...keep]
+    else
+      new_files = all_new_files
+    end
+
+    [ new_files, new_files_destination ]
 
   end
 
   private
 
-  def sync_files(new_files, destination)
+  def sync_new_files
 
-    new_files.each do |file|
-      next if File.exists?("#{destination}/#{File.basename(file)}")
-      cmd = "rsync -vax#{@pretend ? 'n' : ''} \"#{file}\" \"#{destination}\""
-      puts "+ Syncing '#{File.basename(file)}' to '#{destination}'" if @verbose
-      output = `#{cmd}`
-      puts "# #{cmd}" if @debug
+    new_files_destination_parent = '/' + @new_files_destination.split(/\//)[-2]
+
+    raise "Base destination directory '#{new_files_destination_parent}' does not exist" unless File.directory?(new_files_destination_parent)
+
+    puts "DEBUG: new_files_destination_parent=[#{new_files_destination_parent}]" if @debug
+
+    Dir.mkdir(@new_files_destination) if ! File.directory?(@new_files_destination) and ! @pretend
+
+    @new_files.each do |file|
+      destination_file = "#{@new_files_destination}/#{file.gsub(/#{Regexp.escape("#{@source}/")}/, '')}"
+      next if File.exists?(destination_file)
+      puts "+ Copying '#{File.basename(file)}' to '#{destination_file}'" if @verbose
+      puts "DEBUG: Copying '#{file} to '#{destination_file}'" if @debug
+      FileUtils.cp(file, destination_file) unless @pretend
     end
 
   end
 
-  def cleanup(destination, new_files)
+  def cleanup_files_we_dont_want_to_keep
 
-    existing_files = Dir.glob("#{destination}*").sort { |x, y| File::stat(y).ctime <=> File::stat(x).ctime }.reject do |file|
-      new_files.collect { |x| File.basename(x) }.include?(File.basename(file))
+    existing_files = Dir.glob("#{@new_files_destination}/**/*").reject { |x| File.directory?(x) }.sort { |x, y| File::stat(y).ctime <=> File::stat(x).ctime }.reject do |file|
+      @new_files.collect { |x| File.basename(x) }.include?(File.basename(file))
     end
 
     existing_files.each do |file|
-      puts "! Removing '#{file}'"
+      puts "! Removing '#{file}'" if @verbose
       FileUtils.rm_f(file) unless @pretend
     end
 
---
+++ spec/android_sync_spec.rb
@@ -0,0 +1,134 @@
+require 'rubygems'
+require 'rspec'
+require 'fileutils'
+require 'fakefs'
+require 'fakefs/safe'
+require 'fakefs/spec_helpers'
+require 'pp'
+
+require File.dirname(__FILE__) + '/../lib/android_sync'
+
+describe "Android sync" do
+
+  include FakeFS::SpecHelpers
+
+  before :each do
+
+    @source = "/source"
+    @destination = '/destination'
+
+    FileUtils.mkdir_p @source
+    FileUtils.mkdir_p @destination
+
+    @s_files = [
+      "show/a.mp3",
+      "show/b.mp3",
+      "show/c.mp3",
+      "show/d.mp3",
+      "show/e.mp3",
+      "show/f.mp3",
+      "show/g.mp3"
+    ]
+
+    @source_files = @s_files.collect { |x| x = "#{@source}/#{x}" }.each do |s|
+      f = File.new(s, 'w')
+      f.write('0' * (rand(500) + 500))
+      f.close 
+    end
+
+    @d_files = [
+      "show/z.mp3",
+      "show/y.mp3",
+      "show/x.mp3"
+    ]
+
+    FakeFS.deactivate!
+    opts = {
+      :destination => @destination,
+      :verbose => false
+    }
+
+    @a = AndroidSync.new(opts)
+    FakeFS.activate!
+
+  end
+
+  it "should raise Exception 'Destination must be defined' when initialised without options" do
+    lambda {  AndroidSync.new }.should raise_exception('Destination must be defined')
+  end
+
+  it "should return shows to copy from /source/show to /destination/show, keeping last three shows" do
+    new_files, new_files_destination = @a.get_new_files_to_sync(@source, @destination, 3)
+    source_files = Dir.glob("#{@source}/**/*").reject { |x| File.directory?(x) }.sort { |x, y| File::stat(y).ctime <=> File::stat(x).ctime }
+    new_files.should =~ source_files[0...3]
+    new_files_destination.should == @destination
+  end
+
+  it "should copy shows in /source/show/ to /destination/show/" do
+    @a.perform_sync(@source, @destination)
+
+    destination_files = Dir.glob("#{@destination}/**/*").reject { |x| File.directory?(x) }.collect { |x| x = x.gsub(/#{Regexp.escape("#{@destination}/")}/, '') }
+    source_files = Dir.glob("#{@source}/**/*").reject { |x| File.directory?(x) }.collect { |x| x = x.gsub(/#{Regexp.escape("#{@source}/")}/, '') }
+
+    destination_files.should =~ source_files
+  end
+
+  describe "" do
+
+    before :each do
+      @new_destination_files = @d_files.collect { |x| x = "#{@destination}/#{x}" }.each do |s|
+        f = File.new(s, 'w')
+        f.write('0' * (rand(500) + 500))
+        f.close 
+      end
+
+      @new_destination_files.should =~ Dir.glob("#{@destination}/**/*").reject { |x| File.directory?(x) }
+    end
+
+    it "should clean up shows in /destination/show/ that aren't in /source/show/ when keep is defined" do
+
+      @a.perform_sync(@source, @destination, 3)
+
+      destination_files = Dir.glob("#{@destination}/**/*").reject { |x| File.directory?(x) }.collect { |x| x = x.gsub(/#{Regexp.escape("#{@destination}/")}/, '') }
+      source_files = Dir.glob("#{@source}/**/*").reject { |x| File.directory?(x) }.sort { |x, y| File::stat(y).ctime <=> File::stat(x).ctime }.collect { |x| x = x.gsub(/#{Regexp.escape("#{@source}/")}/, '') }
+
+      destination_files.should =~ source_files[0...3]
+    end
+
+    it "should clean up shows in /destination/show/ that aren't in /source/show/ when keep is defined using run()" do
+
+    android_sync_yaml_data = <<-eos
+sources:
+  - label: Test
+    source: '/source/show'
+    destination: "\#{destination_base}/show"
+    keep: 3
+    eos
+
+      android_sync_yaml = '/tmp/android_sync.yml'
+
+      f = File.new(android_sync_yaml, 'w');
+      f.write(android_sync_yaml_data)
+      f.close
+
+      opts = {
+        :destination => @destination,
+        :config => android_sync_yaml,
+        :verbose => false
+      }
+
+      @b = AndroidSync.new(opts)
+      @b.run
+
+      destination_files = Dir.glob("#{@destination}/**/*").reject { |x| File.directory?(x) }.collect { |x| x = x.gsub(/#{Regexp.escape("#{@destination}/")}/, '') }
+      source_files = Dir.glob("#{@source}/**/*").reject { |x| File.directory?(x) }.sort { |x, y| File::stat(y).ctime <=> File::stat(x).ctime }.collect { |x| x = x.gsub(/#{Regexp.escape("#{@source}/")}/, '') }
+
+      destination_files.should =~ source_files[0...3]
+    end
+  end
+
+  after :each do
+    [ @source, @destination ].each { |x| y = x.gsub(/(^\/*|\/*$)/, '').split(/\//).shift ; FileUtils.rm_rf "/#{y}" if y }
+  end
+
+end
